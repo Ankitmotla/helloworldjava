c0fda80d0e14c8f448d8b191558572c7f3b91732
Ankitmotla <95778928+Ankitmotla@users.noreply.github.com>
2022-01-20 00:58:27 +0530

    Update helloworldjava.java

--- helloworldjava.java
+++ helloworldjava.java
@@ -1,6 +1,6 @@
 public class helloworldjava {
    public static void main(String[] args) {
-    System.out.println("Hello World from DevOps_3");
+    System.out.println("Webhook testing");
     }
 }
 
